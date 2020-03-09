e474f27e20ab5878a1036352f784f5193599c6c8
James Stamps Jr <jstampshere@gmail.com>
2020-03-09 23:24:01 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,3 +1,26 @@
+function getIt() {
+  $('p')
+  
+}
+
+function frameIt() {
+  
+}
+
+function pressIt() {
+  
+}
+
+
+
+function submitIt() {
+  
+}
+
+
+
+
+
 //define functions here
 
 $(document).ready(function(){
