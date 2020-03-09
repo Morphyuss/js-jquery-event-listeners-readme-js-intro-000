44ef7941ba17e0abff05f431ad68d346679815aa
James Stamps Jr <jstampshere@gmail.com>
2020-03-09 23:42:14 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -9,34 +9,15 @@ function frameIt() {
   $('img').on("load", function(){
     $('img').addClass('tasty');
     
-  })
+  });
   
 }
 
 
-
-/* $(function() {
-  $('#about').addClass('expand');
-});
-
-$(document).ready(function(){
- $('#about').addClass('expand');
-});
-
-
-$(document).ready(function(){
-  $("button").click(function(){
-    $("h1, h2, p").addClass("blue");
-    $("div").addClass("important");
-  });
-});
-
-
-
-
-
-*/
 function pressIt() {
+  $('input').on("keydown", function() {
+    
+  })
   
 }
 
